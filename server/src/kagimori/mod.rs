d0711b81807ac0c45bfa8b0c011501e594911775
SiLeader <sileader.dev@gmail.com>
2025-08-07 20:57:51 +0900

    add migrate api

--- server/src/kagimori/mod.rs
+++ server/src/kagimori/mod.rs
@@ -18,7 +18,7 @@ use crate::kms::DEK_KEY;
 use crate::proto::kinorca::kagimori::v1::kagimori_key_management_service_server::KagimoriKeyManagementService;
 use crate::proto::kinorca::kagimori::v1::{
     DecryptRequest, DecryptResponse, EncryptRequest, EncryptResponse, GetInformationRequest,
-    GetInformationResponse,
+    GetInformationResponse, MigrateRequest, MigrateResponse,
 };
 use audit_log::AuditLogger;
 use encryption::{Ciphertext, Encryptor, RequestInfo};
@@ -31,9 +31,56 @@ pub(crate) struct KagimoriService<L> {
     encryptor: Encryptor<L>,
 }
 
-impl<L> KagimoriService<L> {
+impl<L> KagimoriService<L>
+where
+    L: 'static + AuditLogger,
+{
     pub(crate) fn new(encryptor: Encryptor<L>) -> Self {
         Self { encryptor }
+    }
+
+    async fn encrypt_impl(&self, request: EncryptRequest) -> Result<Ciphertext, Status> {
+        self.encryptor
+            .encrypt(
+                RequestInfo {
+                    event_id: Uuid::now_v7().to_string(),
+                    service: request.service,
+                    user: request.uid,
+                    data_key: None,
+                },
+                &request.plaintext,
+            )
+            .await
+            .debug_log()
+            .map_err(|e| Status::internal(format!("Internal: {e:?}")))
+    }
+
+    async fn decrypt_impl(&self, request: DecryptRequest) -> Result<Vec<u8>, Status> {
+        let dek = request
+            .annotations
+            .get(DEK_KEY)
+            .ok_or(Status::invalid_argument(
+                "annotations must contain dek.kagimori.kinorca.com",
+            ))?
+            .clone();
+
+        self.encryptor
+            .decrypt(
+                RequestInfo {
+                    event_id: Uuid::now_v7().to_string(),
+                    service: request.service,
+                    user: request.uid,
+                    data_key: None,
+                },
+                Ciphertext {
+                    key_id: request.kek_id,
+                    ciphertext: request.ciphertext,
+                    dek,
+                },
+            )
+            .await
+            .debug_log()
+            .map_err(|e| Status::internal(format!("Internal: {e:?}")))
     }
 }
 
@@ -46,6 +93,7 @@ where
         &self,
         _request: Request<GetInformationRequest>,
     ) -> Result<Response<GetInformationResponse>, Status> {
+        info!("KagimoriKeyManagementService::GetInformation");
         Ok(GetInformationResponse {
             version: "kagimori.kinorca.com/v1".to_string(),
             kek_id: self.encryptor.get_key_id(),
@@ -57,70 +105,57 @@ where
         &self,
         request: Request<EncryptRequest>,
     ) -> Result<Response<EncryptResponse>, Status> {
+        info!("KagimoriKeyManagementService::Encrypt");
         let req = request.into_inner();
 
-        let ciphertext = self
-            .encryptor
-            .encrypt(
-                RequestInfo {
-                    event_id: Uuid::now_v7().to_string(),
-                    service: req.service,
-                    user: req.uid,
-                    data_key: None,
-                },
-                &req.plaintext,
-            )
-            .await
-            .debug_log()
-            .map_err(|e| Status::internal(format!("Internal: {e:?}")))?;
-
-        Ok(EncryptResponse {
-            ciphertext: ciphertext.ciphertext,
-            kek_id: ciphertext.key_id,
-            annotations: HashMap::from([(DEK_KEY.to_string(), ciphertext.dek)]),
-        }
-        .into())
+        self.encrypt_impl(req).await.map(|c| {
+            EncryptResponse {
+                ciphertext: c.ciphertext,
+                kek_id: c.key_id,
+                annotations: HashMap::from([(DEK_KEY.to_string(), c.dek)]),
+            }
+            .into()
+        })
     }
 
     async fn decrypt(
         &self,
         request: Request<DecryptRequest>,
     ) -> Result<Response<DecryptResponse>, Status> {
-        info!("v2.KeyManagementService.Decrypt called");
+        info!("KagimoriKeyManagementService::Decrypt");
         let req = request.into_inner();
 
-        let key_id = Uuid::parse_str(&req.kek_id)
-            .map_err(|e| Status::invalid_argument(format!("Invalid key_id: {e:?}")))?;
-        if !self.encryptor.contains_key(&key_id) {
-            return Err(Status::not_found("key not found"));
+        self.decrypt_impl(req)
+            .await
+            .map(|plaintext| DecryptResponse { plaintext }.into())
+    }
+
+    async fn migrate(
+        &self,
+        request: Request<MigrateRequest>,
+    ) -> Result<Response<MigrateResponse>, Status> {
+        info!("KagimoriKeyManagementService::Migrate");
+        let mut responses = Vec::new();
+        for req in request.into_inner().requests {
+            let service = req.service.clone();
+            let uid = req.uid.clone();
+
+            let plaintext = self.decrypt_impl(req).await?;
+
+            let ciphertext = self
+                .encrypt_impl(EncryptRequest {
+                    plaintext,
+                    service,
+                    uid,
+                })
+                .await?;
+            responses.push(EncryptResponse {
+                ciphertext: ciphertext.ciphertext,
+                kek_id: ciphertext.key_id,
+                annotations: HashMap::from([(DEK_KEY.to_string(), ciphertext.dek)]),
+            });
         }
 
-        let dek = req
-            .annotations
-            .get(DEK_KEY)
-            .ok_or(Status::invalid_argument(
-                "annotations must contain dek.kagimori.kinorca.com",
-            ))?
-            .clone();
-
-        let plaintext = self
-            .encryptor
-            .decrypt(
-                RequestInfo {
-                    event_id: Uuid::now_v7().to_string(),
-                    service: req.service,
-                    user: req.uid,
-                    data_key: None,
-                },
-                Ciphertext {
-                    key_id: req.kek_id,
-                    ciphertext: req.ciphertext,
-                    dek,
-                },
-            )
-            .await
-            .debug_log()
-            .map_err(|e| Status::internal(format!("Internal: {e:?}")))?;
-        Ok(DecryptResponse { plaintext }.into())
+        Ok(MigrateResponse { responses }.into())
     }
 }
